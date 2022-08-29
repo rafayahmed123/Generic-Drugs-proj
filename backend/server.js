82645de4d94bb81fde3772998e6c6df51c6604ef
Rafay Ahmed <rafay@Rafays-MacBook-Pro.local>
2022-08-29 18:31:49 -0400

    database connected

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = 3000;
+const mongoose = require("mongoose");
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
-});
+const CONNECTION_URL =
+"mongodb+srv://pharma-drugs.qzgvvb3.mongodb.net/myFirstDatabase";
+const PORT = process.env.PORT || 3001;
+
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+  )
+  .catch((error) => console.log("error: " + error.message));
